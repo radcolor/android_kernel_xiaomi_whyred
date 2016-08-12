864c16f79fa6be329ba8fab70bbf65b4d1d21649
vincent_tew <vincent_tew@htc.com>
2016-08-12 20:47:06 +0800

    sound: codec: improve impedance sense accuracy and change debounce time
    1. increse impedance ramp to 2x
    2. change debounce time to 512ms
    
    BUG: 30227745
    
    Change-Id: I8d3408e7e8946478484e711be5df63aff2dc9622
    Signed-off-by: vincent_tew <vincent_tew@htc.com>

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -662,7 +662,7 @@ static void wcd_mbhc_report_plug(struct wcd_mbhc *mbhc, int insertion,
 
 		mbhc->hph_type = WCD_MBHC_HPH_NONE;
 		mbhc->zl = mbhc->zr = 0;
-		pr_debug("%s: Reporting removal %d(%x)\n", __func__,
+		pr_info("%s: Reporting removal %d(%x)\n", __func__,
 			 jack_type, mbhc->hph_status);
 		wcd_mbhc_jack_report(mbhc, &mbhc->headset_jack,
 				mbhc->hph_status, WCD_MBHC_JACK_MASK);
@@ -2388,8 +2388,8 @@ static int wcd_mbhc_initialise(struct wcd_mbhc *mbhc)
 		/* Insertion debounce set to 48ms */
 		WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_INSREM_DBNC, 4);
 	} else {
-		/* Insertion debounce set to 96ms */
-		WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_INSREM_DBNC, 6);
+		/* Insertion debounce set to 512ms */
+		WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_INSREM_DBNC, 0x0B);
 	}
 
 	/* Button Debounce set to 16ms */
