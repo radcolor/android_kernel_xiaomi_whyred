4d480adc143534fca5589e7adb9b06f423ae7f56
LuK1337 <priv.luk@gmail.com>
2018-06-16 15:44:41 +0200

    Revert "qcacld-3.0: Don't fill any WMA request if target is not ready"
    This reverts commit 90440e88db493efb6a302ce4aee536f0a83f550e.
    
    Change-Id: I3d54b010da265c381ea2152b6e38ea5a5a986759

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_driver_ops.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_driver_ops.c
@@ -1444,7 +1444,6 @@ static void wlan_hdd_pld_uevent(struct device *dev,
 	mutex_lock(&hdd_init_deinit_lock);
 	switch (uevent->uevent) {
 	case PLD_RECOVERY:
-		cds_set_target_ready(false);
 		hdd_pld_ipa_uc_shutdown_pipes();
 		wlan_hdd_purge_notifier();
 		qdf_complete_wait_events();
