224a257df5f65ac86bf0843987c2b8824f091485
Yaroslav Furman <yaro330@gmail.com>
2018-06-17 02:35:05 +0300

    schedutil: set up/down_rate_limit(-s) to proper values
    Since I don't have any support from ramdisk,
    I need to set everything up myself.
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -695,9 +695,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		tunables->down_rate_limit_us = policy->down_transition_delay_us;
 	} else {
 		unsigned int lat;
-
-                tunables->up_rate_limit_us = LATENCY_MULTIPLIER;
-                tunables->down_rate_limit_us = LATENCY_MULTIPLIER;
+		tunables->up_rate_limit_us = 500;
+		tunables->down_rate_limit_us = 20000;
 		lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 		if (lat) {
                         tunables->up_rate_limit_us *= lat;
