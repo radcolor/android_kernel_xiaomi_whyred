41a15110da98cf3219d453fbe246ef7af3d679ad
Sultan Alsawaf <sultan@kerneltoast.com>
2019-02-23 09:58:24 -0800

    cpuset: Adjust requested cpusets to accommodate for offline CPUs
    Userspace's carefully-tuned cpusets can get mangled when some CPUs are
    deliberately kept offline. To accommodate for this, adjust requested
    cpusets so that the propagated cpumask has the same number of
    cluster-respective online CPUs in it as the requested cpumask.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1760,6 +1760,61 @@ out_unlock:
 	return retval ?: nbytes;
 }
 
+static char *cpuset_online_adjust(char *adj_cpulist, char *orig_cpulist)
+{
+	unsigned int nr_lp_cpus, nr_perf_cpus;
+	cpumask_t orig_mask, tmp;
+	int cpu;
+
+	if (num_online_cpus() == num_present_cpus())
+		return orig_cpulist;
+
+	if (cpulist_parse(orig_cpulist, &orig_mask))
+		return orig_cpulist;
+
+	cpumask_and(&tmp, &orig_mask, cpu_lp_mask);
+	nr_lp_cpus = cpumask_weight(&tmp);
+
+	cpumask_and(&tmp, &orig_mask, cpu_perf_mask);
+	nr_perf_cpus = cpumask_weight(&tmp);
+
+	*adj_cpulist = '\0';
+
+	if (nr_lp_cpus) {
+		for_each_cpu_and(cpu, cpu_lp_mask, cpu_online_mask) {
+			sprintf(adj_cpulist, "%s,%d", adj_cpulist, cpu);
+			if (!--nr_lp_cpus)
+				break;
+		}
+	}
+
+	if (nr_perf_cpus) {
+		for_each_cpu_and(cpu, cpu_perf_mask, cpu_online_mask) {
+			sprintf(adj_cpulist, "%s,%d", adj_cpulist, cpu);
+			if (!--nr_perf_cpus)
+				break;
+		}
+	}
+
+	return adj_cpulist + 1;
+}
+
+static ssize_t cpuset_write_cpus_resmask(struct kernfs_open_file *of,
+					 char *buf, size_t nbytes, loff_t off)
+{
+	char adj_cpulist[NR_CPUS * 2 + 1];
+	BUILD_BUG_ON(NR_CPUS >= 10);
+
+	/*
+	 * Adjust the requested cpuset to only use online CPUs. This is useful
+	 * for the assumption that CPUs which aren't online right now will never
+	 * be online.
+	 */
+	buf = cpuset_online_adjust(adj_cpulist, strstrip(buf));
+
+	return cpuset_write_resmask(of, buf, nbytes, off);
+}
+
 /*
  * These ascii lists should be read in a single call, by using a user
  * buffer large enough to hold the entire map.  If read in smaller
@@ -1852,7 +1907,7 @@ static struct cftype files[] = {
 	{
 		.name = "cpus",
 		.seq_show = cpuset_common_seq_show,
-		.write = cpuset_write_resmask,
+		.write = cpuset_write_cpus_resmask,
 		.max_write_len = (100U + 6 * NR_CPUS),
 		.private = FILE_CPULIST,
 	},
