aa78a55188241963f603305edb23ff83f27b3e40
Danny Lin <danny@kdrag0n.dev>
2019-05-24 21:58:16 -0700

    ACPICA: Fix failed target detection with bare-metal compiler
    The target environment macro checks in ACPICA fail when the compiler
    targets bare-metal environments (i.e. plain ELF images) instead of Linux
    userspace environments. The predefined __linux__ macro is not defined in
    such cases, but this is all ACPICA checks for. Not being able to detect
    the target environment causes hundreds of compile errors to be emitted
    due to missing definitions, because those definitions are normally in
    target-specific headers.
    
    Fix the issue by using the proper method of detecting whether code is
    being compiled as part of the kernel (__KERNEL__) in the ACPICA target
    environment detection header, thus resulting in the appropriate target
    headers being included.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>
    Signed-off-by: nysascape <nysadev@raphielgang.org>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>

--- include/acpi/platform/acenv.h
+++ include/acpi/platform/acenv.h
@@ -170,7 +170,18 @@
  *
  *****************************************************************************/
 
-#if defined(_LINUX) || defined(__linux__)
+#if defined(__GNUC__) && !defined(__INTEL_COMPILER)
+#include <acpi/platform/acgcc.h>
+
+#elif defined(_MSC_VER)
+#include "acmsvc.h"
+
+#elif defined(__INTEL_COMPILER)
+#include <acpi/platform/acintel.h>
+
+#endif
+
+#if defined(_LINUX) || defined(__linux__) || defined(__KERNEL__)
 #include <acpi/platform/aclinux.h>
 
 #elif defined(_APPLE) || defined(__APPLE__)
