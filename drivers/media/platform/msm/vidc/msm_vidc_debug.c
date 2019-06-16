a0b9e713a241796bb0d3f6efb2099d50bf0a9c28
0ctobot <return.of.octobot@gmail.com>
2019-06-16 16:08:51 -0400

    treewide: Don't warn about debugfs failure if disabled
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    [wight554: backport to 4.4]
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- drivers/media/platform/msm/vidc/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.c
@@ -216,7 +216,9 @@ struct dentry *msm_vidc_debugfs_init_core(struct msm_vidc_core *core,
 	snprintf(debugfs_name, MAX_DEBUGFS_NAME, "core%d", core->id);
 	dir = debugfs_create_dir(debugfs_name, parent);
 	if (IS_ERR_OR_NULL(dir)) {
+#ifdef CONFIG_DEBUG_FS
 		dprintk(VIDC_ERR, "Failed to create debugfs for msm_vidc\n");
+#endif
 		goto failed_create_dir;
 	}
 
@@ -399,7 +401,9 @@ struct dentry *msm_vidc_debugfs_init_inst(struct msm_vidc_inst *inst,
 
 	dir = debugfs_create_dir(debugfs_name, parent);
 	if (IS_ERR_OR_NULL(dir)) {
+#ifdef CONFIG_DEBUG_FS
 		dprintk(VIDC_ERR, "Failed to create debugfs for msm_vidc\n");
+#endif
 		goto exit;
 	}
 
