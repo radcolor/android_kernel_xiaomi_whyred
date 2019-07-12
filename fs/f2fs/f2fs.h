a347edd8dcc0c41cdf3a86d14f08a6f5ba8fbf86
Parvinder Singh <psndna88@gmail.com>
2019-07-13 01:10:16 +0530

    f2fs: fix deadlock issue with f2fs (arter97)
    as reported by arter97 here:
    https://forum.xda-developers.com/showpost.php?p=70492947&postcount=8793
    
    revert: "f2fs: reset default idle interval value"
    
    The default value of idle interval is 2 mins, but for most time when
    screen shutdown, there are still operations during the 2 mins interval,
    and gc's sleep time is about 30 secs to 60 secs, so there is almost no
    chance for GC thread to do garbage collecting.
    
    Set default value of idle interval value from 2 mins to 5 secs for
    fixing.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -256,7 +256,7 @@ enum {
 #define DEF_DISCARD_URGENT_UTIL		80	/* do more discard over 80% */
 #define DEF_MAX_DISCARD_URGENT_ISSUE_TIME	10000	/* 10 s, if no candidates on high utilization */
 #define DEF_CP_INTERVAL			60	/* 60 secs */
-#define DEF_IDLE_INTERVAL		5	/* 5 secs */
+#define DEF_IDLE_INTERVAL		120	/* 2 mins */
 #define DEF_DISABLE_INTERVAL		5	/* 5 secs */
 #define DEF_DISABLE_QUICK_INTERVAL	1	/* 1 secs */
 #define DEF_UMOUNT_DISCARD_TIMEOUT	5	/* 5 secs */
