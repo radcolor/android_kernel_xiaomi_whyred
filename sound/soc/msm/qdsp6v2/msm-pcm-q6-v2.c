179a0d7b7f4e43400d5bc936a2c4a49654c62885
Shashi Kant Maurya <smaury@codeaurora.org>
2019-11-11 12:12:24 +0530

    ASoC: msm-pcm: Fix compilation error
    Declare required variable which causing compilation
    error.
    
    Change-Id: I12700aa7117b153c6adc6ee93f9646fc3d12a199
    Signed-off-by: Shashi Kant Maurya <smaury@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.c
@@ -831,6 +831,7 @@ static int msm_pcm_playback_close(struct snd_pcm_substream *substream)
 	struct snd_soc_pcm_runtime *soc_prtd = substream->private_data;
 	struct msm_audio *prtd = runtime->private_data;
 	uint32_t timeout;
+	struct msm_plat_data *pdata = NULL;
 	int dir = 0;
 	int ret = 0;
 
