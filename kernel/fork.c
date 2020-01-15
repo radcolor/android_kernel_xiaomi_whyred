93c6f46d50d1afc5ed5be11d09bdf173d5b90c15
rad <theradcolor@gmail.com>
2020-01-15 20:18:07 +0530

    kernel: boost cpu for 100ms whenzygote is forked
    Signed-off-by: rad <theradcolor@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1796,9 +1796,9 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost CPU to the max for 50 ms when userspace launches an app */
+	/* Boost CPU to the max for 100 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
-		cpu_input_boost_kick_max(50);
+		cpu_input_boost_kick_max(100);
 	}
 
 	/*
