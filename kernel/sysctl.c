d51ac833091a428f330018eebce924ddc2e0954b
Shashank Baghel <theradcolor@gmail.com>
2020-09-23 15:24:43 +0530

    kernel/sysctl: Move sched_wakeup_granularity_ns and sched_child_runs_first to SCHED_DEBUG
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -313,6 +313,13 @@ static struct ctl_table kern_table[] = {
 #endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
+		.procname	= "sched_child_runs_first",
+		.data		= &sysctl_sched_child_runs_first,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
+	{
 		.procname	= "sched_min_granularity_ns",
 		.data		= &sysctl_sched_min_granularity,
 		.maxlen		= sizeof(unsigned int),
@@ -320,6 +327,15 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= sched_proc_update_handler,
 		.extra1		= &min_sched_granularity_ns,
 		.extra2		= &max_sched_granularity_ns,
+	},
+	{
+		.procname	= "sched_wakeup_granularity_ns",
+		.data		= &sysctl_sched_wakeup_granularity,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= sched_proc_update_handler,
+		.extra1		= &min_wakeup_granularity_ns,
+		.extra2		= &max_wakeup_granularity_ns,
 	},
 	{
 		.procname	= "sched_latency_ns",
